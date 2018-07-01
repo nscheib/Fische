2199ef9ac994d64d7cb823c541b263675cb1747e
Nick <33756326+nscheib@users.noreply.github.com>
2018-07-01 16:07:50 +0200

    Delete HerbivoreExeption.java

--- Exception/HerbivoreExeption.java
+++
@@ -1,9 +0,0 @@
-package Exception;
-
-public class HerbivoreExeption extends FressException{
-
-    public HerbivoreExeption(String message){
-        super(message);
-    //throw new HerbivoreException("Was zappelt da im Mund? Raus mit dem einbrecher");
-    }
-}
