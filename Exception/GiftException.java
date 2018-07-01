160ccd4583fd7cdfd579fb37bd0a3a22e1478b24
Nick <33756326+nscheib@users.noreply.github.com>
2018-07-01 16:07:46 +0200

    Delete GiftException.java

--- Exception/GiftException.java
+++
@@ -1,9 +0,0 @@
-package Exception;
-
-public class GiftException extends FressException{
-
-    public GiftException(String message){
-        super(message);
-    //throw new GiftException("Dieser Leckerbissen haelt eine Ueberaschung parat. Er ist giftig und tötet seinen Mörder");
-    }
-}
