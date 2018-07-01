7c2c32c1368058aa0facd707ddf996d24df8b7ed
Nick <33756326+nscheib@users.noreply.github.com>
2018-07-01 16:07:38 +0200

    Delete EkelException.java

--- Exception/EkelException.java
+++
@@ -1,9 +0,0 @@
-package Exception;
-
-public class EkelException extends FressException{
-
-    public EkelException(String message){
-        super(message);
-//throw new EkelException("Ein Biss, eine Grimasse und einmal wieder ausgespuckt. Es wurde noch nie etwas derart wiederliches gegessen!");
-    }
-}
