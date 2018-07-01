d9a98b80d42df2a28502ccd356fc4119da3a62a7
Nick <33756326+nscheib@users.noreply.github.com>
2018-07-01 16:07:33 +0200

    Delete CarnivoreException.java

--- Exception/CarnivoreException.java
+++
@@ -1,9 +0,0 @@
-package Exception;
-
-public class CarnivoreException extends FressException{
-
-    public CarnivoreException(String message){
-        super(message);
-        // throw new CarnivoreException("Ein wahrer Fleischfresser gibt sich nicht mit Kompost zufrieden");
-    }
-}
