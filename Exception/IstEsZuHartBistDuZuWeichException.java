bbba210ba26e09f2596826ae407632e481e95a65
Nick <33756326+nscheib@users.noreply.github.com>
2018-07-01 16:07:55 +0200

    Delete IstEsZuHartBistDuZuWeichException.java

--- Exception/IstEsZuHartBistDuZuWeichException.java
+++
@@ -1,9 +0,0 @@
-package Exception;
-
-public class IstEsZuHartBistDuZuWeichException extends FressException{
-
-    public IstEsZuHartBistDuZuWeichException(String message){
-        super(message);
-    //throw new IstEsZuHartBistDuZuWeichException("Der Panzer ist zu hart oder seine Zähne zu weich. Das war wohl nichts.");
-    }
-}
