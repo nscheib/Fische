a5d54a4191a9f93de5f348be29c2edfa7713841e
Nick <33756326+nscheib@users.noreply.github.com>
2018-07-01 16:08:06 +0200

    Delete ZuGrossException.java

--- Exception/ZuGrossException.java
+++
@@ -1,9 +0,0 @@
-package Exception;
-
-public class ZuGrossException extends FressException{
-
-    public ZuGrossException(String message){
-        super(message);
-//throw new ZuGrossException("Ein kleines Zwicken, dass aber Ignoriert werden kann. 'Warte hat dieser Pimpf gerade versucht mich zu fressen?'");
-    }
-}
